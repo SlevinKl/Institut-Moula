43951e629502bc1818042d5f1f5ee8791819ff7a
Rayane Moula <moula.rayane@gmail.com>
2022-06-27 23:25:02 +0200

    seed v0

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "open-uri"
+
+#### URL avatar
+rayane = URI.open("https://drive.google.com/file/d/1ZuddiOl9Hadn0TDFMaVHIhuETZVkS5gN/view?usp=sharing")
+
+#### clean the database
+Event.destroy_all
+User.destroy_all
+
+#### user creation
+user_rayane = User.new(email: "rayane@moula.fr", password: "azerty", first_name: "Rayane", last_name: "Lewagon")
+user_rayane.avatar.attach(io: rayane, filename: "rayane")
+user_rayane.save!
