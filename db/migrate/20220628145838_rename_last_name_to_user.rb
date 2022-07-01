f1a4c5a83f1da199940404b031ea9f61f18a9f24
Rayane Moula <moula.rayane@gmail.com>
2022-07-01 14:59:27 +0200

    new db with setup device

--- db/migrate/20220628145838_rename_last_name_to_user.rb
+++
@@ -1,5 +0,0 @@
-class RenameLastNameToUser < ActiveRecord::Migration[7.0]
-  def change
-    rename_column :users, :Last_name, :last_name
-  end
-end
--- db/migrate/20220628151944_add_default_value_to_date_in_article.rb
+++
@@ -1,5 +0,0 @@
-class AddDefaultValueToDateInArticle < ActiveRecord::Migration[7.0]
-  def change
-    change_column :articles, :date, :date, default: DateTime.now
-  end
-end
