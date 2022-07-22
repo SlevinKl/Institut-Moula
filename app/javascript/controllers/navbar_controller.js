a6b6246665c59827532c6af20f49aed3339ccec5
tonysnickers <antoine.mendy@hotmail.fr>
2022-07-22 12:23:44 +0200

    terminé

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -28,27 +28,3 @@ export default class extends Controller {
 
 
 
-<%# <nav class="navbar" data-controller="navbar"> %>
-
-        <%#= image_tag 'https://arnaudvallee.com/thumbs/1200x800c/2016-02/1456305881_logoinstitutdebeaute.jpg', class: 'logo-img'%>
-
-    <%# <ul class="nav-menu" data-navbar-target="nav">
-        <li class="nav-item"> %>
-            <%#= link_to 'ACCEUIL', root_path, class: 'nav-link', data: { action: 'click->navbar#close' } %>
-        <%# </li>
-        <li class="nav-item"> %>
-            <%# = link_to 'PRESTATIONS', prestations_path, class: 'nav-link', data: { action: 'click->navbar#close' }
-        </li>
-        <li class="nav-item">
-            <%#= link_to 'CONTACT', new_contact_path, class: 'nav-link', data: { action: 'click->navbar#close' } %>
-        <%# </li>
-        <li class="nav-item"> %>
-            <%#= link_to 'BLOG', articles_path, class: 'nav-link', data: { action: 'click->navbar#close' } %>
-        <%# </li> %>
-    <%# </ul>
-    <div class="hamburger" data-action="click->navbar#menu" data-navbar-target="hamburger">
-        <span class="bar"></span>
-        <span class="bar"></span>
-        <span class="bar"></span>
-    </div> 
-<%# </nav> %>
