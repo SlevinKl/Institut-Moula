ffb3a3af200845f5fd1b00dd378dbb7252e8a1b2
tonysnickers <antoine.mendy@hotmail.fr>
2022-07-22 11:53:58 +0200

    nav responsive terminé

--- db/schema.rb
+++ db/schema.rb
@@ -65,10 +65,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_19_132352) do
     t.text "content"
     t.string "price"
     t.string "category"
+    t.string "time"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "time"
     t.index ["user_id"], name: "index_prestations_on_user_id"
   end
 
